bcd4c335fed426e2ebf602d2fe415f2cd82e4edd
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-01-07 01:43:56 +0700

    fix: change url backend url to be public

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// https://backend-master-production-5633.up.railway.app
+// https://backend-master-production-ec14.up.railway.app/
 // http://localhost:5292
 
 export const axiosClient = axios.create({
-    baseURL: "https://encephalomyelitic-klara-trickiest.ngrok-free.dev",
+    baseURL: "https://backend-master-production-ec14.up.railway.app",
     headers: {
         "Content-Type": "application/json",
     },
